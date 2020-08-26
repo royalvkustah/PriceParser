4e0bb6f4fac6592c131e2bfae8ee1e64fac22196
www-bory72@yandex.ru <www-bory72@yandex.ru>
2020-08-26 20:30:50 +0300

    Добавьте файлы проекта.

---
+++ PriceParser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace PriceParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            Console.WriteLine("ищем цену");
+            while (true)
+            {
+                Updater updater = new Updater();
+                string result = updater.Update("https://info.binance.com/en/currencies/tronix");
+                Task.Delay(2000).Wait();
+                ClearCurrentConsoleLine();
+                Console.Write("цена равна: " + result);
+                Task.Delay(5000).Wait();
+            }
+        }
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            for (int i = 0; i < Console.WindowWidth; i++)
+                Console.Write(" ");
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+    }
+}
---
+++ PriceParser/Updater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net;
+using CsQuery;
+
+
+namespace PriceParser
+{
+    class Updater
+    {
+
+        public string Update(string url)
+        {
+            WebClient wc = new WebClient();
+            string Responte = wc.DownloadString(url);
+            int num = Responte.IndexOf("$");
+            int numb2 = Responte.IndexOf("%",num);
+            string persent = Responte.Substring(numb2-5, 6);
+            if ((persent[0] != '+')&&(persent[0] != '-'))
+            {
+                persent = Responte.Substring(numb2 - 6, 7);
+            }
+            string price = Responte.Substring(num+1,8);
+            string res = price + "$_________" + persent;
+            return res;
+            // return "цена равна: "+Rate+" $";
+        }
+
+
+    }
+}
